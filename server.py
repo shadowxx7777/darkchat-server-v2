8a9a6a85d401759ab2fc820ebe10f6a3484052ba
shadowxx7777 <mskfhideism@gmail.com>
2026-05-31 01:54:03 +0300

    Update server.py

--- server.py
+++ server.py
@@ -514,4 +514,4 @@ init_db()
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     log.info(f"🚀 DarkChat running on port {port}")
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
